b3eecdb2f5105b36eb831c8de65aeadd3df56629
shrestic <nhatphongcgp@gmail.com>
2025-03-12 00:33:17 +0700

    Add TimeStampedModel for Product model

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from model_utils.models import TimeStampedModel
 
-# Create your models here.
+class Product(TimeStampedModel):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
