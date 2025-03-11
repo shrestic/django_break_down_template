e972ab6013551b87531467c637fc296b9e0c769c
shrestic <nhatphongcgp@gmail.com>
2025-03-11 23:25:04 +0700

    Add product list API caching with django-redis

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import CacheTestView
+from .views import CacheTestView, ProductListView
 
 urlpatterns = [
     path('cache-test/', CacheTestView.as_view(), name='cache-test'),
+    path('products/', ProductListView.as_view(), name='product-list'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,8 @@
 from django.core.cache import cache
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from .models import Product
+from .serializers import ProductSerializer
 
 
 class CacheTestView(APIView):
@@ -8,3 +10,16 @@ class CacheTestView(APIView):
         cache.set("test_key", "Hello Redis", timeout=60)
         value = cache.get("test_key")
         return Response({"value": value})
+
+
+class ProductListView(APIView):
+    def get(self, request):
+        cache_key = "product_list"
+        cached_data = cache.get(cache_key)
+        if cached_data:
+            return Response(cached_data)
+
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        cache.set(cache_key, serializer.data, timeout=300)  # Cache 5 phút
+        return Response(serializer.data)
