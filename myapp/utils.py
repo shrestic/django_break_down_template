eb15215adac0c2458a041012a478f7718c033813
shrestic <nhatphongcgp@gmail.com>
2025-03-11 22:26:02 +0700

    Xác minh mật khẩu

--- myapp/utils.py
+++ myapp/utils.py
@@ -1,13 +1,19 @@
 from argon2 import PasswordHasher
 
-
 def hash_password(password: str) -> str:
     ph = PasswordHasher()
     return ph.hash(password)
 
+def verify_password(stored_hash: str, input_password: str) -> bool:
+    ph = PasswordHasher()
+    try:
+        return ph.verify(stored_hash, input_password)
+    except:
+        return False
 
-# Test cơ bản
+# Test
 if __name__ == "__main__":
     password = "mypassword123"
     hashed = hash_password(password)
-    print(f"Hashed password: {hashed}")
+    is_valid = verify_password(hashed, "mypassword123")
+    print("Valid!" if is_valid else "Invalid!")
