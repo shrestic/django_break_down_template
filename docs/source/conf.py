8dc573facfa82fe4026d7c1e04bd3d4129f1d712
shrestic <nhatphongcgp@gmail.com>
2025-03-13 15:20:13 +0700

    add sphinx

---
+++ docs/source/conf.py
@@ -0,0 +1,38 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os
+import sys
+import django
+
+sys.path.insert(0, os.path.abspath("../../"))  # Đưa project root vào sys.path
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mainkode_example.settings")  # thay bằng tên project thực tế
+django.setup()
+
+project = "MainKode Project"
+copyright = "2025, shrestic"
+author = "shrestic"
+release = "0.1"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.napoleon",  # nếu docstring theo Google/NumPy style
+]
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "alabaster"
+html_static_path = ["_static"]
---
+++ docs/source/myapp.rst
@@ -0,0 +1,12 @@
+MyApp API Documentation
+========================
+
+.. automodule:: myapp.models
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+.. automodule:: myapp.views
+   :members:
+   :undoc-members:
+   :show-inheritance:
