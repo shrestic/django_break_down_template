94878d80a7862583cd81717e86796ae63c9cb692
shrestic <nhatphongcgp@gmail.com>
2025-03-13 10:37:01 +0700

    Done

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
+class HelloView(APIView):
+    def get(self, request):
+        return Response({"message": "Hello from DRF with Werkzeug!"})
